abceae8337ee5b42b497b51d7c3a8fed2aecfd75
Wellington Nascimento <wellingto_07@hotmail.com>
2022-06-19 15:24:42 -0300

    feat: init api strapi

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '26a319363947d391ecc1d5cdf986dd97'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '81e395f1-1be8-4c33-90eb-f542c023f350'
+};
